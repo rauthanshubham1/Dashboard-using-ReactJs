ccab800fe0459331664902d50ef5b98a90e13865
Shubham Rauthan <codexshubham@gmail.com>
2023-06-11 15:34:38 +0530

    Designed all the charts and progresscircle, statbox for dashboard

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import Contacts from "./scenes/contacts"
 import Form from "./scenes/form"
 import Calendar from "./scenes/calendar"
 import FAQ from "./scenes/faq"
-// import Bar from "./scenes/bar"
-// import Line from "./scenes/line"
-// import Pie from "./scenes/pie"
-// import Geography from "./scenes/geography"
+import Bar from "./scenes/bar"
+import Pie from "./scenes/pie"
+import Line from "./scenes/line"
+import Geography from "./scenes/geography"
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -35,10 +35,10 @@ function App() {
               <Route exact path="/form" element={<Form />}></Route>
               <Route exact path="/calendar" element={<Calendar />}></Route>
               <Route exact path="/faq" element={<FAQ />}></Route>
-              {/* <Route exact path="/bar" element={<Bar />}></Route> */}
-              {/* <Route exact path="/line" element={<Line />}></Route> */}
-              {/* <Route exact path="/pie" element={<Pie />}></Route> */}
-              {/* <Route exact path="/geography" element={<Geography />}></Route> */}
+              <Route exact path="/bar" element={<Bar />}></Route>
+              <Route exact path="/pie" element={<Pie />}></Route>
+              <Route exact path="/line" element={<Line />}></Route>
+              <Route exact path="/geography" element={<Geography />}></Route>
             </Routes>
 
           </main>
